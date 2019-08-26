94b3f9e0bc45532418ea489a7dfa839c1f1cebf3
Tao Tao <lambortao@gmail.com>
2019-08-26 23:41:37 +0800

    配置antd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React  from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Admin from './Admin';
 
 ReactDom.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
